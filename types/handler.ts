5c85685b5cee28dc382ceb11f386d8d4c4a449f0
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-06-06 09:35:26 +0100

    chore: configurables and basic log

--- types/handler.ts
+++ types/handler.ts
@@ -38,6 +38,16 @@ export type HandlerInterface = {
   testRpcPerformance(): Promise<JsonRpcProvider | null>;
 };
 
+type ModuleName = "[RPCHandler Provider Proxy] -> ";
+
+type ProxySettings = {
+  retryCount: number;
+  retryDelay: number;
+  logTier: "info" | "warn" | "error";
+  appName?: ModuleName & string;
+  logFunction?: (message: string, obj: unknown) => void;
+};
+
 export type HandlerConstructorConfig = {
   networkId: NetworkId;
   networkName: NetworkName | null;
@@ -46,6 +56,7 @@ export type HandlerConstructorConfig = {
   cacheRefreshCycles: number | null;
   runtimeRpcs: string[] | null;
   rpcTimeout: number | null;
+  proxySettings: ProxySettings;
 };
 
 export type NetworkRPCs = typeof networkRpcs;
